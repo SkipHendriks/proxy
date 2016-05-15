33cbb56b3036178eadebf705ba83d008cb09bca8
s.hendriks <s.hendriks@omnihis.nl>
2016-05-15 19:40:07 +0200

    Changed Url's

--- proxy.js
+++ proxy.js
@@ -16,14 +16,16 @@ var proxy_test = new httpProxy.createProxyServer({
 });
 
 http.createServer(function(req, res) {
-    if (req.headers.host === 'http://www.domain.com') {
+    var path = require('url').parse(req.url).path;
+    console.log(path);
+    if (path.indexOf('/timeservice') >= 0) {
         proxy_timeservice.proxyRequest(req, res);
         proxy_timeservice.on('error', function(err, req, res) {
             if (err) console.log(err);
             res.writeHead(500);
             res.end('Oops, something went very wrong...');
         });
-    } else if (req.headers.host === 'http://api.domain.com') {
+    } else if (path.indexOf('/test') >= 0) {
         proxy_test.proxyRequest(req, res);
         proxy_test.on('error', function(err, req, res) {
             if (err) console.log(err);
