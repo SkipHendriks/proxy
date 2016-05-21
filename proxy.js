4188d380dd48f7fb8a701684e50982d9eff1cbd7
s.hendriks <s.hendriks@omnihis.nl>
2016-05-21 20:05:34 +0200

    Removed console.log instances

--- proxy.js
+++ proxy.js
@@ -18,7 +18,6 @@ var proxy_whoami = new httpProxy.createProxyServer({
 
 http.createServer(function(req, res) {
     var path = require('url').parse(req.url).path;
-    console.log(path);
     if (path.indexOf('/timeservice') >= 0) {
         proxy_timeservice.proxyRequest(req, res);
         proxy_timeservice.on('error', function(err, req, res) {
@@ -27,7 +26,6 @@ http.createServer(function(req, res) {
             res.end('Oops, something went very wrong...');
         });
     } else if (path.indexOf('/whoami') >= 0) {
-        console.log('matched');
         proxy_whoami.proxyRequest(req, res);
         proxy_whoami.on('error', function(err, req, res) {
             if (err) console.log(err);
